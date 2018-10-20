739de70ebc6c48dddcf90240d436ea0ada894dc6
Alfikholili <36968508+Alfikholili@users.noreply.github.com>
2018-10-20 21:16:56 +0700

    Create prism.js

---
+++ prism.js
@@ -0,0 +1,16 @@
+<script>
+$('pre').attr('class', 'line-numbers');
+Prism.hooks.add("after-highlight",function(e){var t=e.element.parentNode;if(!t||!/pre/i.test(t.nodeName)||t.className.indexOf("line-numbers")===-1){return}var n=1+e.code.split("\n").length;var r;lines=new Array(n);lines=lines.join("<span></span>");r=document.createElement("span");r.className="line-numbers-rows";r.innerHTML=lines;if(t.hasAttribute("data-start")){t.style.counterReset="linenumber "+(parseInt(t.getAttribute("data-start"),10)-1)}e.element.appendChild(r)})
+</script>
+<script type='text/javascript'>
+var pres = document.getElementsByTagName(&quot;pre&quot;);
+for (var i = 0; i &lt; pres.length; i++) {
+  pres[i].addEventListener(&quot;dblclick&quot;, function () {
+    var selection = getSelection();
+    var range = document.createRange();
+    range.selectNodeContents(this);
+    selection.removeAllRanges();
+    selection.addRange(range);
+  }, false);
+}
+</script>
